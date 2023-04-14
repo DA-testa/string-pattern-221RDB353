ad6beaa4822e6ce5f65baccfe81a0659c4739faf
Pāvels Kudrjavcevs 221RDB353 <125793235+221RDB353@users.noreply.github.com>
2023-04-14 13:17:40 +0000

    main

--- hash_substring.py
+++ hash_substring.py
@@ -1,73 +1,59 @@
-PRIME = 101
-BASE = 256
+def read_file_input(filename):
+    try:
+        with open(f"./tests/{filename}") as f:
+            contents = f.readlines()
+    except FileNotFoundError:
+        raise ValueError("File not found")
+    except:
+        raise ValueError("Error reading file")
+    
+    pattern = contents[0].strip()
+    text = contents[1].strip()
+    
+    return pattern, text
 
+def read_user_input():
+    pattern = input("Enter the pattern: ").rstrip()
+    text = input("Enter the text: ").rstrip()
+    
+    return pattern, text
 
 def read_input():
-    # Lasīt pirmo ievadi - datu tipu un pēc tam, ja nepieciešams, citus ievades parametrus
-    t = input().rstrip()
-
-    # Ja datu tips ir I, lasīt raksta un teksta ievades parametrus
-    if t == 'I':
-        p = input().rstrip()
-        txt = input().rstrip()
-
-    # Ja datu tips ir F, ielasīt faila saturs
-    elif t == 'F':
-        filename = "06"
-
-        # Pārbaudiet faila pēdējo simbolu un pārliecinieties, vai tas ir pareizais fails
-        if str(filename[-1]) != "a":
-            try:
-                with open(f"./tests/06") as f:
-                    contents = f.readlines()
-            except FileNotFoundError:
-                print("File not found")
-                return
-            except:
-                print("Error reading file")
-                return
-
-            p = contents[0].strip()
-            txt = contents[1].strip()
-
-        # Ja fails ir nederīgs, izvadīt kļūdas paziņojumu
-        else:
-            print("Invalid filename")
-            return
-
-    # Atgriezt raksta un teksta ievades parametrus
-    return p, txt
-
-
+    input_type = input("Enter input type (I for user input, F for file input): ").rstrip()
+    
+    if input_type == 'I':
+        return read_user_input()
+    elif input_type == 'F':
+        filename = input("Enter file name: ").rstrip()
+        if str(filename[-1]) == "a":
+            raise ValueError("Invalid filename")
+        return read_file_input(filename)
+    else:
+        raise ValueError("Invalid input type")
 def print_occurrences(output):
-    # Izdrukājiet visus saskaņošanās indeksus
     print(' '.join(map(str, output)))
 
 
-def find_occurrences(p, txt):
-    # Izveidojiet raksta un teksta skaitļu pārstājēju un inicializējiet tos
-    p_hash = 0
-    txt_hash = 0
+def get_occurrences(pattern, text):
+    prime = 101
+    base = 256
 
-    # Aprēķina raksta un teksta skaitļu pārst
-    # # Iterējiet cauri rakstam un teksta teksta pēc bāzes vērtības un izmantojiet mod p, lai iegūtu skaitļu pārstājēju
-    for i in range(len(p)):
-      p_hash = (p_hash * BASE + ord(p[i])) % PRIME
-      txt_hash = (txt_hash * BASE + ord(txt[i])) % PRIME
+    pattern_hash = 0
+    text_hash = 0
+    for i in range(len(pattern)):
+        pattern_hash = (pattern_hash * base + ord(pattern[i])) % prime
+        text_hash = (text_hash * base + ord(text[i])) % prime
 
-# Izveidojiet tukšu sarakstu, lai saglabātu saskaņošanās indeksus
     occurrences = []
-    for i in range(len(txt) - len(p) + 1):
+    for i in range(len(text) - len(pattern) + 1):
+        if pattern_hash == text_hash:
+            if pattern == text[i:i+len(pattern)]:
+                occurrences.append(i)
+        if i < len(text) - len(pattern):
+            text_hash = ((text_hash - ord(text[i]) * (base**(len(pattern)-1))) * base + ord(text[i+len(pattern)])) % prime
 
-    # Ja raksta un teksta skaitļu pārstājēji ir vienādi, salīdziniet raksta un teksta virknes, lai pārbaudītu, vai tās ir identiskas
-      if p_hash == txt_hash:
-          if p == txt[i:i+len(p)]:
-              occurrences.append(i)
+    return occurrences
 
-    # Ja raksta un teksta skaitļu pārstājēji nav vienādi, izmantojiet Rabin-Karp algoritmu, lai atjaunotu teksta skaitļu pārstājēju
-    if i < len(txt) - len(p):
-        txt_hash = ((txt_hash - ord(txt[i]) * (BASE**(len(p)-1))) * BASE + ord(txt[i+len(p)])) % PRIME
-        return occurrences
 
 if __name__ == '__main__':
-    print_occurrences(find_occurrences(*read_input()))
+    print_occurrences(get_occurrences(*read_input()))
